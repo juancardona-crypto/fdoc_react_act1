29e1ed893ca623dfdf7d26a43a049be9644a767b
Juan Diego Cardona <juancardona1611@gmail.com>
2025-10-24 10:16:35 -0500

    componente corregido

--- src/app/componentes/SaludoConDefaultProps.jsx
+++ src/app/componentes/SaludoConDefaultProps.jsx
@@ -1,9 +1,12 @@
+{/*Props: nombre (string, opcional).
+Debe tener valor por defecto para nombre (en JS: valor por defecto en parámetro o fallback interno; evita defaultProps).
+Caso: cuando no se pasa nombre, muestra "Invitado".*/}
 import React from 'react'
 
-export default function SaludoConDefaultProps(props) {
+export default function SaludoConDefaultProps({nombre = "Invitado"}) {
   return (
     <div>
-        <h1>Hola, {props.nombre}</h1>
+        <h1>Hola, {nombre}</h1>
     </div>
   )
 }
